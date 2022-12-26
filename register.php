a0398f5e49f9363695007dd920598e4f84cf65dc
Rolan Zakirov <89405318+Zak618@users.noreply.github.com>
2022-12-26 14:24:37 +0300

    php-auth

---
+++ register.php
@@ -0,0 +1,42 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация и регистрация</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+    <!-- Форама регистрации -->
+    <form action="vendor/signup.php" method="post" enctype="multipart/form-data">
+        <label>ФИО</label>
+        <input type="text" name="full_name" placeholder="Введите своё полное имя">
+        <label>Логин</label>
+        <input type="text" name="login" placeholder="Введите свой логин">
+        <label>Почта</label>
+        <input type="email" name="email" placeholder="Введите свой email">
+        <label>Изображение профиля</label>
+        <input type="file" name="avatar">
+        <label>Пароль</label>
+        <input type="password" name="password" placeholder="Введите свой пароль">
+        <label>Подтверждение пароля</label>
+        <input type="password" name="password_confirm" placeholder="Подтвердите пароль">
+        <button type="submit">Войти</button>
+        <p>
+            У вас уже есть аккаунт? - <a href="index.php">Авторизация</a>
+        </p>
+
+        
+        <?php 
+            if($_SESSION['message']){
+                echo '<p class="msg"> ' . $_SESSION['message'] . ' </p>';
+            }                
+            unset($_SESSION['message']);
+        ?>
+       
+    </form>
+</body>
+</html>
